9b9315484f99436dc4ee3642d49be0b13eef1f79
willbraken <brandon@willbraken.com>
2022-07-11 19:48:17 -0500

    add working grid elements

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 //make flexbox for horizontal rows
 //make flexbox containing horizontal flexbox rows
 
-const containerAll = $('div.containerall');
-const horizontalRow = document.createElement('div');
-horizontalRow.classList.add('horizontalrow');
-
-
+const containerAll = document.getElementById('containerall');
+ 
 
 addItems(16);
 
 function addItems(size) {
     for (i=1; i<=size; i++) {
-        containerAll.append(horizontalRow);
+        const horizontalRow = document.createElement('div');
+        horizontalRow.classList.add('horizontalrow');
+        containerAll.appendChild(horizontalRow);
+        for (j=1; j<=size; j++) {
+            const colorDiv = document.createElement('div');
+            colorDiv.classList.add('colordiv');
+            horizontalRow.appendChild(colorDiv);
+        }
+        
     }
 }
-
